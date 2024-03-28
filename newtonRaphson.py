bfed9b2e5aed7c3877817224a12aebe982fd0c7b
Tair Huri <129064817+TairHuri@users.noreply.github.com>
2024-03-28 19:29:50 +0200

    Update newtonRaphson.py

--- newtonRaphson.py
+++ newtonRaphson.py
@@ -1,27 +1,31 @@
 from colors import bcolors
+import sympy as sp
 
+def newton_raphson(f, p0, TOL, N):
+    x = sp.symbols('x')  # Define symbol for differentiation
+    df = sp.diff(f(x), x)  # Calculate the derivative of f with respect to x
+    df_func = sp.lambdify(x, df)  # Convert the derivative expression to a callable function
 
-def newton_raphson(f, df, p0, TOL, N=50):
     print("{:<10} {:<15} {:<15} ".format("Iteration", "po", "p1"))
     for i in range(N):
-        if df(p0) == 0:
-            print( "Derivative is zero at p0, method cannot continue.")
-            return
+        if df_func(p0) == 0:
+            print("Derivative is zero at p0, method cannot continue.")
+            return None  # Return None if derivative is zero
 
-        p = p0 - f(p0) / df(p0)
-
-        if abs(p - p0) < TOL:
+        p = p0 - f(p0) / df_func(p0)
+        t = abs(p - p0)
+        if abs(t) < TOL:  # Check if difference between p and p0 is below tolerance
             return p  # Procedure completed successfully
         print("{:<10} {:<15.9f} {:<15.9f} ".format(i, p0, p))
         p0 = p
-    return p
+    return None
 
 
 if __name__ == '__main__':
-    f = lambda x: x**3 - 3*x**2
-    df = lambda x: 3*x**2 - 6*x
-    p0 = -5
-    TOL = 1e-6
+    f = lambda x: x**2 - 5*x + 2
+    p0 = 0
+    TOL = 0.0000001
     N = 100
-    roots = newton_raphson(f, df,p0,TOL,N)
-    print(bcolors.OKBLUE,"\nThe equation f(x) has an approximate root at x = {:<15.9f} ".format(roots),bcolors.ENDC,)
+    roots = newton_raphson(f, p0, TOL, N)
+    if roots is not None:
+        print(bcolors.OKBLUE, "\nThe equation f(x) has an approximate root at x = {:<15.9f} ".format(roots), bcolors.ENDC,)
